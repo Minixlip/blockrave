b312f8c6b065e212a3613c4a1731b28e561d171f
mo shihab <minixliplol1@gmail.com>
2025-09-16 17:24:49 +0100

    feat: implement dropdown menu for mobile navigation and enhance Navbar structure

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -2,21 +2,62 @@
 
 import { useState } from 'react';
 import { SidebarTrigger } from '@/components/ui/sidebar';
-import { FaMagnifyingGlass } from 'react-icons/fa6';
+import { FaArrowPointer, FaMagnifyingGlass } from 'react-icons/fa6';
+
+const links = [
+  { name: 'NEW ARRIVAL', href: '/shop/new-arrival' },
+  { name: 'MENS', href: '/shop/mens' },
+  { name: 'WOMENS', href: '/shop/womens' },
+  { name: 'ABOUT US', href: '/about' },
+];
+
+const MobileMenu = () => {
+  return (
+    <div className="md:hidden">
+      <DropdownMenu>
+        <DropdownMenuTrigger>
+          <GiHamburgerMenu />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {links.map((link) => (
+            <div key={link.name}>
+              <DropdownMenuItem className="flex justify-between">
+                <Link href={link.href}>{link.name}</Link>{' '}
+                <FaArrowAltCircleRight />
+              </DropdownMenuItem>
+              <Separator></Separator>
+            </div>
+          ))}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
+
   return (
     <>
-      <div className="flex justify-between w-screen px-4 py-4 relative h-fit items-center">
+      <div className="flex justify-between w-screen px-8 py-4 relative h-fit items-center">
         <div>
-          <span className="font-extrabold">BLOCKRAVE.</span>
+          <Link href="/">
+            <span className="font-extrabold text-4xl italic">BLOCKRAVE.</span>
+          </Link>
         </div>
         <div className="flex gap-4 tracking-wide font-medium items-center">
-          <button>NEW ARRIVAL</button>
-          <button>MENS</button>
-          <button>WOMENS</button>
-          <button>ABOUT US</button>
+          <div className="hidden md:flex gap-4">
+            {links.map((link) => (
+              <Link
+                key={link.name}
+                className="p-2"
+                href={link.href}
+              >
+                {link.name}
+              </Link>
+            ))}
+          </div>
+          <MobileMenu />
           <hr className="text-black font-bold w-3" />
           <button
             id="search"
@@ -39,6 +80,23 @@ export default function Navbar() {
 
 import { Sheet, SheetContent } from '@/components/ui/sheet';
 import { Input } from '@/components/ui/input';
+import Link from 'next/link';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+  FaArrowAltCircleRight,
+  FaArrowAltCircleUp,
+  FaHamburger,
+} from 'react-icons/fa';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { link } from 'fs';
+import { Separator } from '@/components/ui/separator';
 
 function SearchModal({
   open,
